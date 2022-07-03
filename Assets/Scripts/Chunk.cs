47d3840375858f5236ee4e7380a387ffdf56c083
AlexM <tempthing0@gmail.com>
2022-07-02 22:45:57 -0700

    First commit
    Single chunk working

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour {
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+    byte[,,] voxelMap = new byte[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
+
+    void Start () {
+        world = GameObject.Find("World").GetComponent<World>();
+
+        // Define the chunk content
+        voxelMap = PopulateVoxelMap(voxelMap);
+        
+        // Create the chunk
+        Mesh mesh = CreateChunkMesh(voxelMap);
+        meshFilter.mesh = mesh;
+    }
+
+    bool[,,] PopulateVoxelMap(bool[,,] vMap) {
+        // Loop through each voxel in the chunk
+        for (int y = 0; y < VoxelData.ChunkHeight; y++){
+            for (int x = 0; x < VoxelData.ChunkWidth; x++){
+                for (int z = 0; z < VoxelData.ChunkWidth; z++){
+                    vMap[x, y, z] = 0;
+                }
+            }
+        }
+        return vMap;
+    }
+
+    bool checkVoxel(Vector3 position, bool[,,] vMap){
+        int x = Mathf.FloorToInt(position.x);
+        int y = Mathf.FloorToInt(position.y);
+        int z = Mathf.FloorToInt(position.z);
+
+        bool xCheck = x < 0 || x > VoxelData.ChunkWidth - 1;
+        bool yCheck = y < 0 || y > VoxelData.ChunkHeight - 1;
+        bool zCheck = z < 0 || z > VoxelData.ChunkWidth - 1;
+
+        // Check for out of bounds
+        if (xCheck || yCheck || zCheck){
+            return false;
+        } else {
+            return vMap[x, y, z];
+        }
+    }
+
+    Mesh CreateChunkMesh(bool[,,] vMap){
+        int vertexIndex = 0;
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
+        // Loop through each voxel in the chunk
+        for (int y = 0; y < VoxelData.ChunkHeight; y++){
+            for (int x = 0; x < VoxelData.ChunkWidth; x++){
+                for (int z = 0; z < VoxelData.ChunkWidth; z++){
+                    Vector3 offset = new Vector3(x, y, z);
+                    // Looping though each face on a voxel
+                    for (int i = 0; i < 6; i++){
+                        bool outsideFace = !checkVoxel(offset + VoxelData.faceChecks[i], vMap);
+                        if (outsideFace){
+                            vertices.Add(offset + VoxelData.voxelVerts[VoxelData.voxelTriangles[i, 0]]);
+                            vertices.Add(offset + VoxelData.voxelVerts[VoxelData.voxelTriangles[i, 1]]);
+                            vertices.Add(offset + VoxelData.voxelVerts[VoxelData.voxelTriangles[i, 2]]);
+                            vertices.Add(offset + VoxelData.voxelVerts[VoxelData.voxelTriangles[i, 3]]);
+                            uvs.Add(VoxelData.voxelUvs[0]);
+                            uvs.Add(VoxelData.voxelUvs[1]);
+                            uvs.Add(VoxelData.voxelUvs[2]);
+                            uvs.Add(VoxelData.voxelUvs[3]);
+                            triangles.Add(vertexIndex);
+                            triangles.Add(vertexIndex + 1);
+                            triangles.Add(vertexIndex + 2);
+                            triangles.Add(vertexIndex + 2);
+                            triangles.Add(vertexIndex + 1);
+                            triangles.Add(vertexIndex + 3);
+                            vertexIndex += 4;
+                        }
+                    }
+                }
+            }
+        }
+        Mesh chunkMesh = new Mesh();
+        chunkMesh.vertices = vertices.ToArray();
+        chunkMesh.triangles = triangles.ToArray();
+        chunkMesh.uv = uvs.ToArray();
+        chunkMesh.RecalculateNormals();
+        return chunkMesh;
+    }
+
+    void AddVoxelDataToChunk(Vector3 position) {
+    }
+}
---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VoxelData {
+
+
+    public static readonly int ChunkWidth = 8;
+    public static readonly int ChunkHeight = 16;
+
+    // Lookup for the vectors of a square
+    public static readonly Vector3[] voxelVerts = new Vector3[8]{
+        new Vector3(1.0f, 1.0f, 1.0f), // 0
+        new Vector3(1.0f, 1.0f, 0.0f), // 1
+        new Vector3(1.0f, 0.0f, 1.0f), // 2
+        new Vector3(1.0f, 0.0f, 0.0f), // 3
+        new Vector3(0.0f, 1.0f, 1.0f), // 4
+        new Vector3(0.0f, 1.0f, 0.0f), // 5
+        new Vector3(0.0f, 0.0f, 1.0f), // 6
+        new Vector3(0.0f, 0.0f, 0.0f), // 7
+    };
+
+    public static readonly Vector3[] faceChecks = new Vector3[6]{
+        new Vector3(0.0f, 1.0f, 0.0f),  // Front
+        new Vector3(0.0f, -1.0f, 0.0f), // Back
+        new Vector3(1.0f, 0.0f, 0.0f),  // Top
+        new Vector3(-1.0f, 0.0f, 0.0f), // Bottom
+        new Vector3(0.0f, 0.0f, -1.0f), // Left
+        new Vector3(0.0f, 0.0f, 1.0f),  // Right
+    };
+
+    // Lookup for the draw patterns on each face
+    public static readonly int[,] voxelTriangles = new int[6, 4] {
+        {0, 1, 4, 5}, // Top face
+        {6, 7, 2, 3}, // Bottom face
+        {0, 2, 1, 3}, // Front face
+        {5, 7, 4, 6}, // Back face
+        {1, 3, 5, 7}, // Left face
+        {4, 6, 0, 2} // Right face
+    };
+
+
+    // Lookup for UV mappings
+    public static readonly Vector2[] voxelUvs = new Vector2[4] {
+        new Vector2(1.0f, 1.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(0.0f, 0.0f)
+    };
+}
